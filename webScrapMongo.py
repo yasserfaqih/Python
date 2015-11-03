6c85a16d05df5f0856f329371a92ca15de97b6f8
Matar <memo430@gmail.com>
2015-11-04 00:47:22 +0300

    using BS4 & MongoDB to scrape a website

---
+++ webScrapMongo.py
@@ -0,0 +1,80 @@
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+import sys
+import os
+
+
+
+# make a request :: 'http://pythonpodcast.com/'
+def makeReq(path) :
+	r = requests.get(path)
+	return r.content
+
+# make a soup
+''' it takes the request response as arug'''
+def makeSoup(html) :
+	soup = BeautifulSoup(html)
+	return soup
+
+
+# get data from the so=oup
+''' it takes a soup as arug'''
+def getData(my_soup) :
+	data = my_soup.find_all("article")
+	return data
+
+
+# get list of dictionaries for all data
+''' it takes data as arug'''
+def prepareData(my_data) :
+
+	# define some holders
+	episode_no = None
+	title = None
+	summary = None
+	link = None
+
+	itemDic = {}
+	itemsList = []
+
+	# produce the items list
+	for item in my_data :
+		episode_no 	= int(item.contents[1].find_all("a",{"rel":"bookmark"})[0].text.encode('utf-8').split()[1])
+		title 		= str(" ".join(item.contents[1].find_all("a",{"rel":"bookmark"})[0].text.encode('utf-8').split()[3:]))
+		summary 	= str(item.contents[3].find_all("p")[0].text.encode('utf-8'))
+		link 		= str(item.contents[5].find_all("a")[0].get("href"))
+
+		itemDic = {'episode_no': episode_no, 'title' : title, 'summary' : summary, 'link':link}
+
+		itemsList.append(itemDic)
+
+	return itemsList
+
+
+# establish a connection to MongoDB
+def connectMongoDB() :
+	connection = pymongo.MongoClient("mongodb://localhost")
+	db = connection.pypodcast
+	collection = db.articles
+	return collection
+
+# insert clean data in MongoDB
+''' it takes items list returned by  prepareData() '''
+def insertData(connection ,clean_data) :
+	for dic in clean_data :
+		connection.insert_one(dic)
+
+
+
+def main() :
+	re = makeReq('http://pythonpodcast.com/')
+	so = makeSoup(re)
+	gd = getData(so)
+	pd = prepareData(gd)
+	cn = connectMongoDB()
+	it = insertData(cn, pd)
+
+if __name__ == '__main__' :
+	main()
+
