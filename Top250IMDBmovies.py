3c09c5c63eff887e6c30b69c7caee30b1c5d9359
Matar <mataralhawiti@users.noreply.github.com>
2015-11-04 16:56:40 +0300

    Update Top250IMDBmovies.py

--- Top250IMDBmovies.py
+++ Top250IMDBmovies.py
@@ -1,1 +1,9 @@
+''' to-do '''
+# scrape Top250 list from IMDB using Requests&BS4 modules
+# Store the data in MongoDB
+# use pymongo module to mainpulate data
+# use Pandas, NumPy to analyze & plot data
+# use IPython
 
+
+''' --- '''
